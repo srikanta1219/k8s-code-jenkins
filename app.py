1e2b81b81ba8dc56d0720e1a89967261023b87a0
srikanta1219 <82170893+srikanta1219@users.noreply.github.com>
2022-01-10 19:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'HEY THIS IS SRIKANTA TESTING GITOPS PROJECT 2!!!'
+    return 'HEY THIS IS SRIKANTA TESTING GITOPS PROJECT Success!!!'
